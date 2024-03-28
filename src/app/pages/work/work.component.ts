42ff882ffcd78ebd0434b4b8545cb5ce96e3b145
bestfriend1 <autanber111@gmail.com>
2024-03-28 16:44:43 +0600

    design page added

---
+++ src/app/pages/work/work.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work',
+  templateUrl: './work.component.html',
+  styleUrls: ['./work.component.scss']
+})
+export class WorkComponent {
+  isActiveCategorySlide = false;
+
+  /**
+   * CATEGORY SLIDE FUNCTIONALITY 
+   * onToggleCategorySlide()
+   */
+
+  onToggleCategorySlide(){
+      this.isActiveCategorySlide = !this.isActiveCategorySlide;
+  }
+}
